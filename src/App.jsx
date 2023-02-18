855218e2c982eebd78124086538f9c1fe2c9474f
Aleksander Simanski Sanin <alesim428@gmail.com>
2023-02-18 10:40:06 -0300

    agregando componente ItemCount

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Fold from './components/Fold';
 import ItemListContainer from './components/ItemListContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <><><NavBar /><Fold /></><ItemListContainer greeting="Nuestros Productos" /></>
+   <BrowserRouter> <><><NavBar /><Fold /></><ItemListContainer greeting="Nuestros Productos" /></></BrowserRouter>
   )
 }
 
