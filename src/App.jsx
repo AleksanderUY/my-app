9bd8bfa097f32a30a1fddfb5b8a24516651bc544
Aleksander Simanski Sanin <alesim428@gmail.com>
2023-02-18 22:51:55 -0300

    agregando componentes Item, ItemList, ItemDetail

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ItemListContainer from './components/ItemListContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-   <BrowserRouter> <><><NavBar /><Fold /></><ItemListContainer greeting="Nuestros Productos" /></></BrowserRouter>
+   <BrowserRouter> <><><NavBar /><Fold /></><ItemListContainer  /></></BrowserRouter>
   )
 }
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ItemCount from './ItemCount';
 
+
+
 const ItemListContainer = (props) => {
   return (
     <><div style={{ padding: 20, backgroundColor: 'lightgray' }}>
       <h2 style={{ textAlign: 'center' }}>{props.greeting}</h2>
     </div>
-    <ItemCount /></>
+    <ItemCount stock={10} /></>
   );
 };
 
